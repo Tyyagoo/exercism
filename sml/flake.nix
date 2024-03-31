746706a889f9702f06d83791a7f782c322b72768
Tyyago <tyyago.dev@gmail.com>
2024-03-31 14:49:23 -0300

    :sparkles: add `sml/hello-world` exercise

---
+++ sml/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "old camel";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [ ];
+      allSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system overlays; };
+      });
+      runPkg = pkgs: pkg: "${pkgs.${pkg}}/bin/${pkg}";
+    in {
+      devShells = forAllSystems ({ pkgs }: with pkgs; {
+        default = mkShell {
+          name = "sml";
+          packages =  [
+            polyml
+
+            (writeScriptBin "test" ''
+              poly -q --use test.sml 
+            '')
+          ];
+        };
+      });
+  };
+}
