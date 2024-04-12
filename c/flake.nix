606f87e0460552a260f6567fecabb4b718a62380
Tyyago <tyyago.dev@gmail.com>
2024-04-12 10:35:33 -0300

    :sparkles: add `c/hello-world` exercise

---
+++ c/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "a wild segfault appears!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [ ];
+      allSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system overlays; };
+      });
+      runPkg = pkgs: pkg: "${pkgs.${pkg}}/bin/${pkg}";
+    in {
+      devShells = forAllSystems ({ pkgs }: with pkgs; {
+        default = mkShell {
+          name = "c";
+          packages =  [
+            gnumake
+            clang
+          ];
+        };
+      });
+  };
+}
