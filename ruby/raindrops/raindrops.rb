f0796cde816d298c672d20465d7404da4c5a4d27
Tyyago <tyyago.dev@gmail.com>
2024-12-05 10:43:29 -0300

    :sparkles: add `ruby/raindrops` exercise

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,11 @@
+class Raindrops
+  class << Raindrops
+    def convert(n)
+      result = (n % 3 == 0 ? "Pling" : "") +
+        (n % 5 == 0 ? "Plang" : "") +
+        (n % 7 == 0 ? "Plong" : "")
+      
+      result.empty? ? n.to_s : result
+    end
+  end
+end
