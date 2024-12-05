e17730ed8d7bf92e1ca7505d18933bf4dc8f20e2
Tyyago <tyyago.dev@gmail.com>
2024-12-05 13:31:46 -0300

    :sparkles: add `rust/roman-numerals` exercise

---
+++ rust/roman-numerals/src/lib.rs
@@ -0,0 +1,36 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::fmt::{Display, Formatter, Result};
+
+pub struct Roman(String);
+
+type NumeralPair = (&'static str, u32);
+
+lazy_static! {
+    static ref NUMERALS: [NumeralPair; 13] = [
+        ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90),
+        ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
+    ];
+}
+
+impl Display for Roman {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
+        write!(_f, "{}", self.0)
+    }
+}
+
+impl From<u32> for Roman {
+    fn from(mut num: u32) -> Self {
+        let mut s = String::with_capacity(7 * 3);
+
+        for &(numeral, value) in NUMERALS.iter() {
+            while num >= value {
+                s.push_str(numeral);
+                num -= value;
+            }
+        }
+
+        Roman(s)
+    }
+}
