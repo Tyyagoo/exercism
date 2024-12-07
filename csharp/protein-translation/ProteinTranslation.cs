817f9cfc005d9d55798768dda7a7398ef7627833
Tyyago <tyyago.dev@gmail.com>
2024-12-07 13:24:54 -0300

    :art: improve `csharp/protein-translation` exercise

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,34 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class ProteinTranslation
 {
-    public static Dictionary<string, string> CodonToProtein { get; private set; }
-
-     static ProteinTranslation() {
-         CodonToProtein = new Dictionary<string, string>
-         {
-             { "AUG", "Methionine" }, { "UUU", "Phenylalanine" },
-             { "UUC", "Phenylalanine" }, { "UUA", "Leucine" },
-             { "UUG", "Leucine" }, { "UCU", "Serine" },
-             { "UCC", "Serine" }, { "UCA", "Serine" },
-             { "UCG", "Serine" },  { "UAU", "Tyrosine" },
-             { "UAC", "Tyrosine" }, { "UGU", "Cysteine" },
-             { "UGC", "Cysteine" }, { "UGG", "Tryptophan" },
-             { "UAA", "STOP" }, { "UAG", "STOP" }, { "UGA", "STOP" }
-         };
-     }
-    
-    public static string[] Proteins(string strand)
-    {
-        List<string> proteins = new List<string> { };
-        for (int i = 0; i < strand.Length; i = i + 3)
+    public static Dictionary<string, string> CodonToProtein { get; } =
+        new Dictionary<string, string>
         {
-            string codon = strand.Substring(i, 3);
-            string protein = ProteinTranslation.CodonToProtein[codon];
-            if (protein == "STOP") break;
-            proteins.Add(protein);
-        }
-        return proteins.ToArray();
-    }
+            { "AUG", "Methionine" }, { "UUU", "Phenylalanine" },
+            { "UUC", "Phenylalanine" }, { "UUA", "Leucine" },
+            { "UUG", "Leucine" }, { "UCU", "Serine" },
+            { "UCC", "Serine" }, { "UCA", "Serine" },
+            { "UCG", "Serine" }, { "UAU", "Tyrosine" },
+            { "UAC", "Tyrosine" }, { "UGU", "Cysteine" },
+            { "UGC", "Cysteine" }, { "UGG", "Tryptophan" },
+            { "UAA", "STOP" }, { "UAG", "STOP" }, { "UGA", "STOP" }
+        };
+
+    public static string[] Proteins(string strand) =>
+        Enumerable.Range(0, strand.Length / 3)
+                  .Select(i => CodonToProtein[strand.Substring(i * 3, 3)])
+                  .TakeWhile(p => p != "STOP")
+                  .ToArray();
 }
