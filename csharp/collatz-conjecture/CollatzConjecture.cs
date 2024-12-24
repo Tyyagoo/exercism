f916ca5f826ddc4ad0d7681303cc86aa0223dfdb
Tyyago <tyyago.dev@gmail.com>
2024-12-23 21:27:59 -0300

    :sparkles: add `csharp/collatz-conjecture` exercise

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1) {
+            throw new ArgumentOutOfRangeException(nameof(number), "Must be >= 1");
+        }
+        int steps = 0;
+        while (number > 1) {
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
+            steps++;
+        }
+        return steps;
+    }
+}
